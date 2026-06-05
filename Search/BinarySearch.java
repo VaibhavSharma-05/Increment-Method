7daf518125abf8020e6639563cc27645ead8bdbb
Vaibhav <flutistvaibhav08@gmail.com>
2026-06-05 15:55:03 +0530

    .

--- Search/BinarySearch.java
+++ Search/BinarySearch.java
@@ -3,28 +3,48 @@ import java.util.*;
 public class BinarySearch {
 
     public static void main(String args[]) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter element : ");
+        System.out.print("Enter element : ");
         int element = sc.nextInt();
 
-        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int arr[] = {54, 66, 44, 55, 33, 77, 87};
 
-        int li = 0;
-        int hi = arr.length - 1;
+        try {
 
-        while (li <= hi) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int j = 0; j < arr.length - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
 
-            int mid = (li + hi) / 2;
-
-            if (arr[mid] == element) {
-                System.out.print("Element found at " + mid + " index position ");
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
             }
-            if (element > arr[mid]) {
-                li = mid + 1;
-            } else {
-                hi = mid - 1;
+
+            int li = 0;
+            int hi = arr.length - 1;
+
+            while (li <= hi) {
+                int mid = (li + hi) / 2;
+
+                if (arr[mid] == element) {
+                    System.out.print("element found at " + mid + " position");
+                    break;
+                }
+                if (element > arr[mid]) {
+                    li = mid - 1;
+                } else {
+                    hi = mid + 1;
+                }
             }
+        }
+
+        catch (Exception e) {
+            System.out.println("Unexpected error: " + e.getMessage());
+        }
+        finally {
+            sc.close();
         }
     }
 }
