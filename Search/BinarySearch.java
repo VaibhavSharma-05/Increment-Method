0c3436feec12f9e39aad2dd4808a722da1f7cc65
Vaibhav <flutistvaibhav08@gmail.com>
2026-06-10 12:26:10 +0530

    .

--- Search/BinarySearch.java
+++ Search/BinarySearch.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class BinarySearch {
 
     public static void main(String args[]) {
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter element : ");
         int element = sc.nextInt();
 
-        int arr[] = {54, 66, 44, 55, 33, 77, 87};
+        int arr[] = {44, 66, 43, 55, 76, 77, 33};
 
         try {
 
@@ -15,9 +16,10 @@ public class BinarySearch {
                 for (int j = 0; j < arr.length - i - 1; j++) {
                     if (arr[j] > arr[j + 1]) {
 
+                        //Swap
                         int temp = arr[j];
-                        arr[j] = arr[j+1];
-                        arr[j+1] = temp;
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
                     }
                 }
             }
@@ -26,24 +28,24 @@ public class BinarySearch {
             int hi = arr.length - 1;
 
             while (li <= hi) {
+
                 int mid = (li + hi) / 2;
 
                 if (arr[mid] == element) {
-                    System.out.print("element found at " + mid + " position");
+                    System.out.print("Element found at : " + mid + " position");
                     break;
                 }
                 if (element > arr[mid]) {
-                    li = mid - 1;
+                    li = mid + 1;
                 } else {
-                    hi = mid + 1;
+                    hi = mid - 1;
                 }
             }
         }
-
-        catch (Exception e) {
-            System.out.println("Unexpected error: " + e.getMessage());
+        catch (Exception e){
+            System.out.print("Unexpected error :" + e.getLocalizedMessage());
         }
-        finally {
+        finally{
             sc.close();
         }
     }
